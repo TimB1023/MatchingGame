e037b825adc1fd812cd80116dc230ac55e05e11f
TimB1023 <39093958+TimB1023@users.noreply.github.com>
2021-08-31 16:45:35 +0200

    Form created and filled with generic Webding shapes as labels Labels randomised Webdings display issue fixed (UseCompatibleTextRendering) Icon colours set to background

--- Form1.cs
+++ Form1.cs
@@ -10,12 +10,52 @@ using System.Windows.Forms;
 
 namespace MatchingGame
 {
-    public partial class Form1 : Form
+    public partial class MatchingGameForm : Form
     {
-        public Form1()
+        public MatchingGameForm()
+            {
+                InitializeComponent();
+                AssignIconsToSquares();
+            }
+
+
+        // Use this Random object to choose random icons for the squares
+        Random random = new Random();
+
+        // Each of these letters is an interesting icon
+        // in the Webdings font,
+        // and each icon appears twice in this list
+        List<string> icons = new List<string>()
+            {
+                "!", "!", "N", "N", ",", ",", "k", "k",
+                "b", "b", "v", "v", "w", "w", "z", "z"
+            };
+
+        
+
+        /// <summary>
+        /// Assign each icon from the list of icons to a random square
+        /// </summary>
+        private void AssignIconsToSquares()
         {
-            InitializeComponent();
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
         }
 
     }
+
+    
 }
